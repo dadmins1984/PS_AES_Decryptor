18f122bffacf2725431a284a49db6f394c7f6ca3
dadmins1984 <116805139+dadmins1984@users.noreply.github.com>
2022-11-02 11:19:01 +0100

    Update decrypt.ps1

--- decrypt.ps1
+++ decrypt.ps1
@@ -35,4 +35,6 @@ Remove-Item $item -Force
 }
 }
 }
+Remove-Item $UserPath\key.s -Force
+Remove-Item $UserPath\IV.s -Force
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
